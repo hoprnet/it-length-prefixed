bb4e726f0e9ac5536e3f7703444b172066160924
achingbrain <alex@achingbrain.net>
2022-07-30 23:24:33 +0100

    chore: zero buffer as we do not always overwrite it all

--- test/helpers/int32BE-encode.ts
+++ test/helpers/int32BE-encode.ts
@@ -1,10 +1,9 @@
 import { Uint8ArrayList } from 'uint8arraylist'
-import { allocUnsafe } from '../../src/alloc.js'
 import type { LengthEncoderFunction } from '../../src/index.js'
 
 export const int32BEEncode: LengthEncoderFunction = (value) => {
   const data = new Uint8ArrayList(
-    allocUnsafe(4)
+    new Uint8Array(4)
   )
   data.setInt32(0, value, false)
 
